2add70864204738576f7e366f7efd4d37e047c5f
Nishant Gandhi <nishant.gandhi@datarobot.com>
2020-05-26 19:33:49 -0400

    part 2

--- 2020-05-26-meetup-6/app.py
+++ 2020-05-26-meetup-6/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
+db = SQLAlchemy(app)
 
 
-@app.route("/")
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html', page_name="home page")
 
