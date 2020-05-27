1dd106c97e402678482ef6ff05a9a3d71fe55497
Nishant Gandhi <nishant.gandhi@datarobot.com>
2020-05-26 20:14:12 -0400

    part 3

--- 2020-05-26-meetup-6/app.py
+++ 2020-05-26-meetup-6/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,7 +16,43 @@ class Todo(db.Model):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', page_name="home page")
+    if request.method == 'POST':
+        task_content = request.form['task_content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception:
+            return "DB operation failed"
+    else:
+        todo_tasks = Todo.query.order_by(Todo.date_added).all()
+        return render_template('index.html', todo_tasks=todo_tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except Exception:
+        return "DB Delete went wrong!!"
+
+
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task_to_update.content = request.form['task_update']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception:
+            return "Something went wrong"
+    else:
+        return render_template('update.html', task_to_update=task_to_update)
 
 
 if __name__ == "__main__":
